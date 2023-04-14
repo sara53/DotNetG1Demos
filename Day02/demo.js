07b45b8036c63ae494bcd00e6fe9c4f4e9a68453
sara53 <sara.atef001@gmail.com>
2023-04-14 11:46:29 +0200

    day02 demo

--- Day02/demo.js
+++ Day02/demo.js
@@ -1,3 +1,92 @@
+/**
+ * str [string API]
+ * ======
+ *  -- length
+ * -----------------
+ *  -- charAt
+ *  -- indexOf [-1]
+ *  -- lastIndexOf
+ *  -- concat
+ *  -- toLowerCase
+ *  -- toUpperCase
+ *  -- trim
+ *  -- trimStart
+ *  -- trimEnd
+ *  -- split
+ *  -- includes
+ *  -- startsWith
+ *  -- endsWith
+ *  -- substring
+ */
+// var str = "hello from iti";
+// var result = str.substring(1, 4);
+
+// console.log(result);
+// var result = str.toUpperCase();
+
+// console.log(str.trimEnd() + "log");
+// console.log(result);
+/**================Array In Js=========== */
+/**
+ * length
+ * ----------------
+ *  == push
+ *  == unshif
+ *  == pop
+ *  == shift
+ *  == concat
+ *  == reverse
+ *  == join
+ *   delete element from any index in arr [Search]
+ */
+
+/**
+ * element Sort
+ * // call backfunction
+ *
+ *
+ *  40-50 == -10
+ *  50-40 -- 10
+ *  40 - 40 -- 0
+ */
+// var arr = [10, 60, 50, 33, 20, 55, 11];
+// arr.sort().reverse();
+// console.log(arr);
+// var studentNames = ["mona", "ali", "ahmed"];
+
+// var output = studentNames.join("@");
+// console.log(studentNames);
+// console.log(output);
+
+// var str = "ahmed"; // demha
+// var result = str.split("").reverse().join("");
+// console.log(result);
+
+// console.log(result.join(""));
+// studentNames.shift();
+// studentNames.shift();
+// studentNames.shift();
+// studentNames.pop();
+// studentNames.pop();
+// studentNames.pop();
+// studentNames.pop();
+
+// studentNames.unshift("ahmed");
+// studentNames.unshift("hoda");
+// studentNames.push("new value");
+// studentNames.push("ahmed");
+// studentNames.push("ali");
+// studentNames.push([true]);
+// set
+// studentNames[0] = "ahmed";
+// studentNames[1] = "alaa";
+// studentNames[2] = [1, 2, 3];
+// // get
+// console.log(studentNames);
+// for (var i = 0; i < studentNames.length; i++) {
+//   console.log(studentNames[i]);
+// }
+
 /** functions
  *  =====================
  *      -- built in functions
