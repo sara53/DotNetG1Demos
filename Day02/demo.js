8f003ac32ccd9faeafc1df592674df0c03db12d1
sara53 <sara.atef001@gmail.com>
2023-04-14 11:34:33 +0200

    demo day02

---
+++ Day02/demo.js
@@ -0,0 +1,82 @@
+/** functions
+ *  =====================
+ *      -- built in functions
+ *      -- user defined functions
+ *             == function statement (declaration)
+ *             == function expression
+ *
+ */
+
+/** function statement */
+// function add(x, y) {
+//   x = x || 0;
+//   y = y || 0;
+
+//   console.log(x + y);
+// }
+// var result = add(10, 30);
+// console.log(result);
+
+// console.log("======");
+// add(3);
+// console.log("==========");
+// add(3, 5);
+// console.log("====");
+// add(10, 22, 143);
+// add(11, 11);
+
+/** function expression  , anonymous function*/
+// var result = function (x, y) {
+//   x = x || 0;
+//   y = y || 0;
+//   console.log(x + y);
+// };
+// result(7, 8);
+
+/**
+ *
+ * clg(fname)
+ * var fname = "mona"
+ */
+
+// console.log(fname);
+// var fname = "mona";
+// console.log(fname);
+// var fname;
+// console.log(fname)
+// fname = "mona";
+// console.log(fname);
+
+// console.log(this);
+// console.log(window);
+
+// console.log(num2);
+// var num = 10;
+
+// var num2 = 11;
+
+// console.log(num + num2);
+
+// add(10, 30);
+// function add(x, y) {
+//   x = x || 0;
+//   y = y || 0;
+
+//   console.log(x + y);
+// }
+// console.log(result());
+
+// var result = function (x, y) {
+//   x = x || 0;
+//   y = y || 0;
+//   console.log(x + y);
+// };
+/**
+ *
+ * call back function (search)
+ */
+
+// 3 --> fizz
+// 5 -> buzz
+// 15 --> fizBuzz
+// 7 -- none
