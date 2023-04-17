4dc8391e26a991f179826906cf0516ecdb1be83d
sara53 <sara.atef001@gmail.com>
2023-04-17 11:24:02 +0200

    Day03 Demo

---
+++ Day03/demo.js
@@ -0,0 +1,118 @@
+// function greet(x, y) {
+//   console.log("Welcome From ITI");
+//   x(y);
+// }
+
+// var trackFun = function (trackName) {
+//   console.log(trackName);
+// };
+
+// greet(trackFun, "DotNet");
+/** ================================= */
+// var test = "ali";
+// function add() {
+//   test = "mona";
+//   var a = 3;
+//   var b = 4;
+//   console.log(test);
+// }
+
+// var result = add();
+
+// console.log(test);
+// console.log(result);
+
+// var sayHello = function (trackName) {
+//   console.log("Welcome ", trackName);
+// };
+
+// function greet() {
+//   sayHello("python");
+//   return "Success Call";
+// }
+
+// var result = greet();
+
+// console.log(result);
+// var person = {
+//   fname: "ali",
+//   age: 30,
+//   address: {
+//     city: "banha",
+//     street: "Saad Str",
+//     code: 12,
+//   },
+//   colors: ["red", "green", "blue"],
+//   display: function (x) {
+//     // method
+//     x();
+//     console.log(person.fname);
+//   },
+// };
+
+// person.display(function () {
+//   console.log("Hello");
+// });
+// console.log(person.display());
+// var x = person.adress["city"];
+
+// console.log(x);
+// person.color = "red";
+// set
+// person.fname = "ahmed"; .... override
+// person["fname"] = "ahmed";
+
+// console.log(person);
+// console.log(person.color);
+
+// get
+// console.log(person.fname);
+// console.log(person["fname"]);
+/**
+ * person : Properties ---> key : value
+ */
+
+/** dotNotation, subscriptNotation */
+
+// var fname = "hello from iti ";
+// fname.charAt(0);
+
+/** Date */
+
+// var myDate = new Date();
+
+// /**
+//  *  sun --> 0
+//  * mon -- 1
+//  *
+//  * ==============
+//  *  jan --> 0
+//  */
+// console.log(myDate.getFullYear());
+/** ================ */
+/**
+ * Math
+ * -------------
+ * PI
+ * ---------------
+ *  == sqrt
+ *  == pow
+ *  == abs
+ *  == sign
+ *  == min
+ *  == max
+ * -----------------
+ *  == round
+ *  == floor
+ *  == ceil
+ *  == random
+ */
+
+/**
+ * positive ==> 1
+ * negative ==> -1
+ *  0 ==> 0
+ *
+ */
+// console.log(Math.sin((90 * Math.PI) / 180));
+// console.log(Math.floor(Math.random() * 10));
